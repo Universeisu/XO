d6d67ecb39066f98a08b16211a432df78728e126
Universeisu <111554684+Universeisu@users.noreply.github.com>
2023-10-20 12:11:46 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+import { TicTacToe } from './components/TicTacToe'
+
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <TicTacToe/>
+    </>
+  )
+}
+
+export default App
